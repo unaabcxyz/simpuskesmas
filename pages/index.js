c261efea7708a758bad181d918217671de7aba18
dwiauliaa <dwiauliiaaaa@gmail.com>
2024-08-22 23:26:53 +0700

    up

--- pages/index.js
+++ pages/index.js
@@ -1,94 +1,95 @@
-import logoPuskesMas from './Image/logoPuskesMas.png';
+// import logoPuskesMas from './Image/logoPuskesMas.png';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import useDeviceSize from '@/components/UseDevice/useDeviceSize';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { useAuth } from '@/lib/hooks/auth';
-const Home=()=>{ 
+const Home = () => {
   const [matches, setMatches] = useState(false);
   const [width] = useDeviceSize();
-  const [styleHeader,setStyleHeader]=useState({
-    logo:{
-      width:50,
+  const [styleHeader, setStyleHeader] = useState({
+    logo: {
+      width: 50,
     },
-    nama_instansi:{ 
-      fontSize:'6px'   
+    nama_instansi: {
+      fontSize: '6px'
     },
-    menu_text:{
-      fontSize:'6px'
+    menu_text: {
+      fontSize: '6px'
     },
-    box_header:{
-      height:'40px'
+    box_header: {
+      height: '40px'
     },
-    container:{
-       width:'75%'
+    container: {
+      width: '75%'
     },
-    box_menu:{
-        width:50
+    box_menu: {
+      width: 50
     }
   })
-  const {loadingAuth,loggedIn}=useAuth();
-  useEffect(()=>{ 
-    if(width>480){
-      desktopView(); 
+  const { loadingAuth, loggedIn } = useAuth();
+  useEffect(() => {
+    if (width > 480) {
+      desktopView();
       setMatches(false)
-    }else{
+    } else {
       mobileView();
       setMatches(true);
     }
-  },[width])
-  const router =useRouter();
-  const mobileView=()=>{ 
-    setStyleHeader({...styleHeader,['logo']:{width:25},
-                                  ['nama_instansi']:{fontSize:'6px'},
-                                  ['menu_text']:{fontSize:'6px'},
-                                  ['box_header']:{height:'40px'},
-                                  ['box_menu']:{width:12},
-                                  ['container']:{width:'75%'}})
-}
-const desktopView=()=>{
-  setStyleHeader({...styleHeader,['logo']:{width:50},
-                              ['nama_instansi']:{fontSize:'14px'},
-                              ['box_menu']:{width:20},
-                              ['menu_text']:{fontSize:'14px'},
-                              ['box_header']:{height:'60px'},
-                              ['container']:{width:'50%'}})
-}
-if(!loadingAuth && loggedIn){
-  router.push('/utama')
-}
-  return(
-    <div className='vh-100 d-flex align-items-center'> 
+  }, [width])
+  const router = useRouter();
+  const mobileView = () => {
+    setStyleHeader({
+      ...styleHeader, ['logo']: { width: 25 },
+      ['nama_instansi']: { fontSize: '6px' },
+      ['menu_text']: { fontSize: '6px' },
+      ['box_header']: { height: '40px' },
+      ['box_menu']: { width: 12 },
+      ['container']: { width: '75%' }
+    })
+  }
+  const desktopView = () => {
+    setStyleHeader({
+      ...styleHeader, ['logo']: { width: 50 },
+      ['nama_instansi']: { fontSize: '14px' },
+      ['box_menu']: { width: 20 },
+      ['menu_text']: { fontSize: '14px' },
+      ['box_header']: { height: '60px' },
+      ['container']: { width: '50%' }
+    })
+  }
+  if (!loadingAuth && loggedIn) {
+    router.push('/utama')
+  }
+  return (
+    <div className='vh-100 d-flex align-items-center'>
       <div className='w-75  mx-auto h-50 rounded border border-2 border-success p-3 pt-4 ps-5 '>
-            {matches==false?( <div className="d-inline-block align-top w-25 align-top">
-                <Image src={logoPuskesMas}
-                          width={styleHeader.logo.width+150}
-                          height={styleHeader.logo.width+150}/>
-            </div>):(<div className="d-inline-block align-top w-100 align-top">
-                <div className="mx-auto" style={{width:'fit-content'}}>
-                  <Image src={logoPuskesMas}
-                            width={styleHeader.logo.width+150}
-                            height={styleHeader.logo.width+150}/>
-                </div>
-                
-            </div>)}
-              <div className="d-inline-block  align-top">
-                  <div className=" h-100 mt-5">
-                    <h1>Puskesmas Rawat Inap Jungkat </h1>
-                    <p style={{fontFamily:'serif'}}>Alamat : Jl. Raya Jungkat, Sei Nipah, Kec. Jongkat, Kab. Mempawah, Kalimantan Barat 78351</p>
-                    <p className="fw-semibold" style={{color:'#024319'}}>Pusat Kesehatan Masyarakat</p>
-                  </div>
-              </div>
-            <div className='mt-5  mx-auto d-block w-25'>
-              <button className="btn btn-primary px-5 w-100 fw-bold "  style={{fontSize:"20px"}}  onClick={()=>{router.push('pendaftaran/')}}>Pendaftaran</button>
-            </div>
-       </div>
-            
+        {matches == false ? (<div className="d-inline-block align-top w-25 align-top">
+          <img src="http://simpuskesmasjungkat.online/iconFoto/logoPuskesmas.png" style={{ width: styleHeader.logo.width + 150 + "px", height: styleHeader.logo.width + 150 + "px", }} />
+        </div>) : (<div className="d-inline-block align-top w-100 align-top">
+          <div className="mx-auto" style={{ width: 'fit-content' }}>
+            <img src="http://simpuskesmasjungkat.online/iconFoto/logoPuskesmas.png" style={{ width: styleHeader.logo.width + 150 + "px", height: styleHeader.logo.width + 150 + "px", }} />
+
+          </div>
+
+        </div>)}
+        <div className="d-inline-block  align-top">
+          <div className=" h-100 mt-5">
+            <h1>Puskesmas Rawat Inap Jungkat </h1>
+            <p style={{ fontFamily: 'serif' }}>Alamat : Jl. Raya Jungkat, Sei Nipah, Kec. Jongkat, Kab. Mempawah, Kalimantan Barat 78351</p>
+            <p className="fw-semibold" style={{ color: '#024319' }}>Pusat Kesehatan Masyarakat</p>
+          </div>
+        </div>
+        <div className='mt-5  mx-auto d-block w-25'>
+          <button className="btn btn-primary px-5 w-100 fw-bold " style={{ fontSize: "20px" }} onClick={() => { router.push('pendaftaran/') }}>Pendaftaran</button>
+        </div>
+      </div>
+
     </div>
-  
-   
-  
+
+
+
   )
 }
 export default Home;
